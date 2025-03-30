0eb95ab18b577864cb500fcd0c11d85f3dd9a111
Rémi Bardon <remi@remibardon.name>
2025-03-30 16:42:15 +0200

    feat(mod_init_admin): Recreate the superadmin account if needed after a reload

--- plugins/prose/mod_init_admin.lua
+++ plugins/prose/mod_init_admin.lua
@@ -10,14 +10,13 @@
 -----------------------------------------------------------
 
 local jid_prepped_split = require "prosody.util.jid".prepped_split;
-local log = require "prosody.util.logger".init("init_admin");
 local um = require "prosody.core.usermanager";
 
 local prosody = _G.prosody;
 local hosts = prosody.hosts;
 
 local function init_admin()
-  log("debug", "Initializing superadmin account…");
+  module:log("debug", "Initializing superadmin account…");
 
   -- Read JID from Prosody configuration
   local jid = module:get_option_string("init_admin_jid");
@@ -27,6 +26,11 @@ local function init_admin()
   local username, host = jid_prepped_split(jid);
   if not (username and host) then
     return false, "Invalid JID. Check `init_admin_jid` in the Prosody configuration file.";
+  end
+
+  if um.user_exists(username, host) then
+    module:log("debug", "Superadmin account already exists.");
+    return true;
   end
 
   local role = "prosody:operator";
@@ -55,24 +59,32 @@ local function init_admin()
       return false, ("Could not create user: %s"):format(err);
     end
 
-    log("info", "Superadmin account created successfully");
+    module:log("info", "Superadmin account created successfully.");
   else
     local ok, err = um.set_jid_role(jid, module.host, role);
     if not ok then
       return false, ("Could not grant role '%s' for host '%s' to '%s': %s"):format(role, module.host, jid, err);
     end
 
-    log("info", ("Superadmin account '%s' was successfully granted the role '%s' for host '%s'"):format(jid, role, host));
+    module:log("info", ("Superadmin account '%s' was successfully granted the role '%s' for host '%s'."):format(jid, role, host));
   end
 
-  return true
+  return true;
+end
+
+function handle_reload()
+  local ok, err = init_admin();
+  if not ok then
+    module:log("error", err);
+  end
 end
 
 -- `module.ready` runs when the module is loaded and the server has finished starting up.
 -- See `core/modulemanager.lua`.
 function module.ready()
-  local ok, err = init_admin();
-  if not ok then
-    log("error", err);
-  end
+	handle_reload();
 end
+
+-- `config-reloaded` runs when the configuration is reloaded.
+-- See `core/modulemanager.lua`.
+module:hook_global("config-reloaded", handle_reload);
