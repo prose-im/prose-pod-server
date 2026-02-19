830cfb6475d21bd011a4589c14e4e6a754e64924
Rémi Bardon <remi@remibardon.name>
2026-02-19 14:13:15 +0700

    fix: Fix `/users-util/stats`

--- api/src/router/users_util.rs
+++ api/src/router/users_util.rs
@@ -23,13 +23,30 @@ pub async fn users_stats(
 
     let mut prosodyctl = backend.prosodyctl.write().await;
 
-    // NOTE: We need to filter out service accounts,
-    //   which don’t have the `prosody:member` role.
-    let user_count = prosodyctl
-        .user_get_jids_with_role(domain, "prosody:member")
-        .await
-        .no_context()?
-        .len();
+    // Filter out service accounts.
+    // NOTE: Given how roles are attributed at the moment,
+    //   `.user_get_jids_with_role(domain, "prosody:member")` doesn’t return
+    //   what we want. `prosody:admin` accounts inherit the `prosody:member`
+    //   role, but it’s not taken into account as it’s not an explicit
+    //   secondary role. As a workaround, we’ll count all `prosody:member`,
+    //   `prosody:admin` and `prosody:operator`. It’s a footgun but we’ll
+    //   improve that when we rework permissions. We could list all users then
+    //   remove the ones which have the `prosody:registered` role, but during
+    //   testing we realized it doesn’t work because of a strange bug in
+    //   Prosody (see https://gist.github.com/RemiBardon/4a62f940376cf707d66fd1b933ed2e2a).
+    //   Until we figure out what’s going wrong there, this will do.
+    let mut user_count = 0;
+    for role in [
+        "prosody:member",
+        "prosody:admin",
+        "prosody:operator",
+    ] {
+        user_count += prosodyctl
+            .user_get_jids_with_role(domain, role)
+            .await
+            .no_context()?
+            .len();
+    }
 
     // Release lock ASAP.
     drop(prosodyctl);
